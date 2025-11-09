cb920ed07b4cef92dcc82fd546cef0d0e1d83320
Nil <nildiazb@gmail.com>
2025-11-09 20:24:18 +0100

    update3

--- server/config/database.js
+++ server/config/database.js
@@ -1,23 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'db_mysql',
-  user: 'root',
-  password: '1234',
-  database: 'projBD',
+  host: process.env.DB_HOST || "db_mysql",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "1234",
+  database: process.env.MYSQL_DATABASE || "projBD",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ Unable to connect to the database:', err);
+    console.error("❌ Unable to connect to the database:", err);
     return;
   }
-  console.log('✅ Connection has been established successfully.');
+  console.log("✅ Connection has been established successfully.");
   connection.release();
 });
 
 module.exports = pool.promise();
-
